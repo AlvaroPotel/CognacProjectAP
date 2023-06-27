2d6a7a665d7453536d6fa9073d1724622a791702
AlvaroPotel <alvaropotel@gmail.com>
2023-06-27 22:27:07 +0100

    Create Routes, create Listing and DetailsDrink pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,23 @@
-
-import Navbar from "./components/navbar";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from "assets/img/pages/Listing";
+import DetailsDrink from "assets/img/pages/DetailsDrink";
+import Navbar from "components/Navbar";
 
 function App() {
   return (
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/detailsdrink">
+          <Route path=":drinkId" element={<DetailsDrink />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/assets/img/pages/Listing/index.tsx
@@ -0,0 +1,9 @@
+import Navbar from 'components/Navbar';
+import './styles.css';
+
+function Listing() {
+    return (
+        <h1>Pagina de listagem</h1>
+      );
+}
+export default Listing;
